a01e7511d07681832128d3d971d58a981662628d
thgus0 <sohyun10521052@gmail.com>
2025-06-04 20:53:35 +0900

    테스트코드 작성 - 할일을 입력할 때 100자 이상 작성하면 입력할 수 없다.

--- zz.home-work/src/components/test/todo-form.test.tsx
+++ zz.home-work/src/components/test/todo-form.test.tsx
@@ -2,20 +2,18 @@ import { render, screen, fireEvent } from '@testing-library/react';
 import { TodoForm } from '../todo-form';
 import { describe, it, expect, vi } from 'vitest';
 
-describe('TodoForm', () => {
-  it('입력값이 있을 때 Add Todo 버튼 활성화', () => {
-    const setTodos = vi.fn();
-    render(<TodoForm todos={[]} setTodos={setTodos} />);
-
-    const todoInput = screen.getByLabelText('New Todo');
-    const dateInput = screen.getByLabelText('Deadline');
-    const addButton = screen.getByRole('button', { name: 'Add Todo' });
-
-    expect(addButton).toBeDisabled();
-
-    fireEvent.change(todoInput, { target: { value: '공부' } });
-    fireEvent.change(dateInput, { target: { value: '2025-06-04' } });
-
-    expect(addButton).not.toBeDisabled();
+describe('TodoForm 테스트', () => {
+    it('100자 이상 입력하면 Add Todo 버튼이 비활성화된다.', () => {
+      const setTodos = vi.fn();
+      render(<TodoForm todos={[]} setTodos={setTodos} />);
+  
+      const todoInput = screen.getByLabelText('New Todo');
+      const dateInput = screen.getByLabelText('Deadline');
+      const addButton = screen.getByRole('button', { name: 'Add Todo' });
+  
+      fireEvent.change(todoInput, { target: { value: 'a'.repeat(101) } });
+      fireEvent.change(dateInput, { target: { value: '2025-06-04' } });
+  
+      expect(addButton).toBeDisabled();
+    });
   });
-});
